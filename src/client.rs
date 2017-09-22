16924f800feb43c96f58ce8c7c58ffcdaec70e52
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-09-23 00:13:16 +0200

    Send ip and hostname in body. Server prints.

--- src/client.rs
+++ src/client.rs
@@ -1,24 +1,62 @@
 extern crate hyper;
 extern crate futures;
 extern crate tokio_core;
+extern crate get_if_addrs;
+#[macro_use]
+extern crate json;
+extern crate hostname;
 
-use std::io::{self, Write};
-use futures::{Future, Stream};
-use hyper::Client;
+use futures::Future;
+use hyper::{Client, StatusCode, Method, Request};
+use hyper::header::{ContentLength, ContentType};
 use tokio_core::reactor::Core;
+
+use std::net::{IpAddr, Ipv4Addr};
+
+fn get_local_ip() -> Option<Ipv4Addr> {
+    get_if_addrs::get_if_addrs().ok().and_then(|addrs| {
+        addrs
+            .iter()
+            .map(|addr| addr.ip())
+            .filter(IpAddr::is_ipv4)
+            .filter(|a| !a.is_loopback())
+            .next()
+    }).map(|ip| match ip {
+        IpAddr::V4(v4) => v4,
+        IpAddr::V6(_) => unreachable!()
+    })
+}
+
+fn ip_to_json(ip: Ipv4Addr) -> json::JsonValue {
+    json::JsonValue::String(format!("{}", ip))
+}
 
 fn main() {
     let mut core = Core::new().unwrap();
     let client = Client::new(&core.handle());
 
     let uri = "http://127.0.0.1:8000".parse().unwrap();
-    let work = client.get(uri).and_then(|res| {
-        println!("response: {}", res.status());
-        res.body().for_each(|chunk| {
-            io::stdout()
-                .write_all(&chunk)
-                .map_err(From::from)
-        })
+    let request: Request = {
+        let payload = {
+            let mut json_obj = object!();
+            if let Some(ip) = get_local_ip() {
+                json_obj["ip"] = ip_to_json(ip);
+            }
+            json_obj["hostname"] = hostname::get_hostname().unwrap().into();
+            json::stringify(json_obj)
+        };
+        let mut req = Request::new(Method::Post, uri);
+        req.headers_mut().set(ContentType::json());
+        req.headers_mut().set(ContentLength(payload.len() as u64));
+        req.set_body(payload);
+        req
+    };
+
+    let work = client.request(request).map(|res| match res.status() {
+        StatusCode::Ok => {}
+        status => {
+            println!("Got other status code back: {:?}", status);
+        }
     });
     core.run(work).unwrap();
 }
--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,12 @@
 extern crate hyper;
 extern crate futures;
+extern crate json;
 
+use futures::Stream;
 use futures::future::Future;
-use hyper::header::ContentLength;
 use hyper::server::{Http, Request, Response, Service};
 
 struct Ping;
-
-const PONG: &'static str = "PONG";
 
 impl Service for Ping {
     type Request = Request;
@@ -17,11 +16,24 @@ impl Service for Ping {
     type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
 
     fn call(&self, _req: Request) -> Self::Future {
-        Box::new(futures::future::ok(
-            Response::new()
-                .with_header(ContentLength(PONG.len() as u64))
-                .with_body(PONG),
-        ))
+        println!("{:#?}", _req);
+        let remote_addr = _req.remote_addr().map(|h| h.to_string());
+        let body = _req.body();
+        Box::new(body.concat2().and_then(move |chunk| {
+            if let Ok(json) = std::str::from_utf8(&*chunk) {
+                let j = json::parse(json).expect("malformed json received");
+                if let Some(ip) = remote_addr {
+                    println!("global ip: {:?}", ip);
+                }
+                if let Some(ip) = j["ip"].as_str() {
+                    println!("local ip: {:?}", ip);
+                }
+                if let Some(hostname) = j["hostname"].as_str() {
+                    println!("hostname: {:?}", hostname);
+                }
+            }
+            futures::future::ok(Response::new())
+        }))
     }
 }
 
